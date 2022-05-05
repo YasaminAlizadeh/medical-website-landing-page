c0d639d425bb115fd78d2e252c102fb6a18f36a2
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-05-05 18:46:46 +0430

    data: added popular searches data to render from

---
+++ assets/data/popular_searches.js
@@ -0,0 +1,157 @@
+const popularSearches = {
+  specialities: [
+    {
+      icon: "./assets/images/popular/depilation.png",
+      title: "Dermatology",
+    },
+    {
+      icon: "./assets/images/popular/medicine.png",
+      title: "Internal Medicine",
+    },
+    {
+      icon: "./assets/images/popular/brain.png",
+      title: "Neurology",
+    },
+    {
+      icon: "./assets/images/popular/lungs.png",
+      title: "General Medicine",
+    },
+    {
+      icon: "./assets/images/popular/molar-tooth.png",
+      title: "Dentistry",
+    },
+    {
+      icon: "./assets/images/popular/nose.png",
+      title: "Otolaryngology",
+    },
+  ],
+
+  conditions: [
+    {
+      icon: "./assets/images/popular/depilation.png",
+      title: "Blood",
+    },
+    {
+      icon: "./assets/images/popular/medicine.png",
+      title: "Cancer and neoplasm",
+    },
+    {
+      icon: "./assets/images/popular/brain.png",
+      title: "Cardiovascular",
+    },
+    {
+      icon: "./assets/images/popular/lungs.png",
+      title: "Congenital disorders",
+    },
+    {
+      icon: "./assets/images/popular/molar-tooth.png",
+      title: "Ear",
+    },
+    {
+      icon: "./assets/images/popular/nose.png",
+      title: "Eye",
+    },
+    {
+      icon: "./assets/images/popular/depilation.png",
+      title: "Infection",
+    },
+    {
+      icon: "./assets/images/popular/medicine.png",
+      title: "Inflammatory and immune system",
+    },
+    {
+      icon: "./assets/images/popular/brain.png",
+      title: "Injuries and accidents",
+    },
+    {
+      icon: "./assets/images/popular/lungs.png",
+      title: "Mental health",
+    },
+    {
+      icon: "./assets/images/popular/molar-tooth.png",
+      title: "Metabolic and endocrine",
+    },
+    {
+      icon: "./assets/images/popular/nose.png",
+      title: "Musculoskeletal",
+    },
+    {
+      icon: "./assets/images/popular/depilation.png",
+      title: "Neurological",
+    },
+    {
+      icon: "./assets/images/popular/medicine.png",
+      title: "Oral and gastrointestina",
+    },
+    {
+      icon: "./assets/images/popular/brain.png",
+      title: "Renal and urogenital",
+    },
+    {
+      icon: "./assets/images/popular/lungs.png",
+      title: "Reproductive health and childbirth",
+    },
+    {
+      icon: "./assets/images/popular/molar-tooth.png",
+      title: "Respiratory",
+    },
+    {
+      icon: "./assets/images/popular/nose.png",
+      title: "Skin",
+    },
+    {
+      icon: "./assets/images/popular/depilation.png",
+      title: "Stroke",
+    },
+    {
+      icon: "./assets/images/popular/medicine.png",
+      title: "Generic health relevance",
+    },
+    {
+      icon: "./assets/images/popular/brain.png",
+      title: "Disputed aetiology and other",
+    },
+  ],
+  procedures: [
+    {
+      icon: "./assets/images/popular/lungs.png",
+      title: "Appendectomy",
+    },
+    {
+      icon: "./assets/images/popular/molar-tooth.png",
+      title: "Cataract Surgery / Refractive Lens Exchange",
+    },
+    {
+      icon: "./assets/images/popular/nose.png",
+      title: "C-Section",
+    },
+    {
+      icon: "./assets/images/popular/depilation.png",
+      title: "CT Scan",
+    },
+    {
+      icon: "./assets/images/popular/medicine.png",
+      title: "Echocardiogram",
+    },
+    {
+      icon: "./assets/images/popular/brain.png",
+      title: "Heart Bypass Surgery",
+    },
+    {
+      icon: "./assets/images/popular/lungs.png",
+      title: "Hip Replacement Surgery",
+    },
+    {
+      icon: "./assets/images/popular/molar-tooth.png",
+      title: "MRI",
+    },
+    {
+      icon: "./assets/images/popular/nose.png",
+      title: "Upper Endoscopy",
+    },
+    {
+      icon: "./assets/images/popular/depilation.png",
+      title: "X-Ray",
+    },
+  ],
+};
