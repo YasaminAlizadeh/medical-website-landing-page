40be484adc949ca247c2de8ea37bf8e29e5df29d
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-05-05 18:54:35 +0430

    feat(popular): add items to list from data array

---
+++ javascript/popular.js
@@ -0,0 +1,59 @@
+const listLabels = document.querySelectorAll(".tab__label");
+const itemsList = document.querySelector("#search-list");
+
+listLabels.forEach((label) => {
+  label.addEventListener("click", () => {
+    listLabels.forEach((label) => label.classList.remove("tab__label--active"));
+
+    label.classList.add("tab__label--active");
+    changePopularItems(label.innerText.toLowerCase());
+  });
+});
+
+const changePopularItems = (label) => {
+  let data;
+  itemsList.innerHTML = "";
+
+  switch (label) {
+    case "specialities":
+      data = popularSearches.specialities;
+      break;
+
+    case "conditions":
+      data = popularSearches.conditions;
+      break;
+
+    case "procedures":
+      data = popularSearches.procedures;
+      break;
+
+    default:
+      break;
+  }
+
+  data.forEach((item) => {
+    const { icon, title } = item;
+
+    const li = document.createElement("li");
+    li.className = "splide__slide tab__item";
+
+    const element = `
+        <a class="splide__slide__container">
+            <div class="item__icon">
+                <img
+                    src="${icon}"
+                />
+            </div>
+                ${title}
+            </a>
+        `;
+
+    li.innerHTML = element;
+
+    itemsList.appendChild(li);
+  });
+
+  splide.refresh();
+};
+
+changePopularItems("specialities");
