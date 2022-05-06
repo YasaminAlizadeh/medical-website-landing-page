232ea0ddc55e05f7f2fc117d07ebd433379cc163
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-05-06 21:18:26 +0430

    add(data): added staff data

---
+++ assets/data/staff.js
@@ -0,0 +1,42 @@
+const staff = [
+  {
+    name: "Dr. Leslie Alexander",
+    image: "./assets/images/staff/no_bg/8.png",
+    speciality: "Dental Surgery",
+  },
+  {
+    name: "Dr. Jerome Bell",
+    image: "./assets/images/staff/no_bg/9.png",
+    speciality: "Pediatric Medicine",
+  },
+  {
+    name: "Dr. Robert Fox",
+    image: "./assets/images/staff/no_bg/2.png",
+    speciality: "Gastroenterologist",
+  },
+  {
+    name: "Dr. Esther Howard",
+    image: "./assets/images/staff/no_bg/3.png",
+    speciality: "Thoracic Surgeon",
+  },
+  {
+    name: "Dr. Albert Flores",
+    image: "./assets/images/staff/no_bg/7.png",
+    speciality: "Intern Neurologist",
+  },
+  {
+    name: "Dr. Kathryn Murphy",
+    image: "./assets/images/staff/no_bg/5.png",
+    speciality: "Obstetrician and Gynecologist",
+  },
+  {
+    name: "Dr. Arlene McCoy",
+    image: "./assets/images/staff/no_bg/6.png",
+    speciality: "Cardiologist",
+  },
+  {
+    name: "Dr. Jeremy Wilson",
+    image: "./assets/images/staff/no_bg/1.png",
+    speciality: "Intern Dermatologist",
+  },
+];
