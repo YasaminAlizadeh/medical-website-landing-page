6ece234401aaa813b1723090e50b8eb4bb0f7611
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-05-07 01:44:58 +0430

    add(reviews): added data array

---
+++ assets/data/reviews.js
@@ -0,0 +1,38 @@
+const reviews = [
+  {
+    name: "Brooklyn Jacob",
+    image: "./assets/images/feedback/feedback_4.png",
+    location: "New York, USA",
+    rating: 5,
+    text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Voluptate cupiditate et, ea id ipsam in fugit ab iure incidunt
+        voluptas delectus similique, vel nesciunt soluta provident
+        recusandae possimus dignissimos eaque.`,
+  },
+  {
+    name: "Matilda Beck",
+    image: "./assets/images/feedback/feedback_4.png",
+    location: "Berlin, Germany",
+    rating: 3,
+    text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Voluptate in fugit ab iure incidunt
+        voluptas delectus similique, vel nesciunt soluta provident
+        recusandae .`,
+  },
+  {
+    name: "Elin Blackburn",
+    image: "./assets/images/feedback/feedback_4.png",
+    location: "Paris, France",
+    rating: 5,
+    text: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Voluptate cupiditatedignissimos eaque.`,
+  },
+  {
+    name: "Tiana Sampson",
+    image: "./assets/images/feedback/feedback_4.png",
+    location: "Toronto, Canada",
+    rating: 4,
+    text: `Lorem ipsum dolor similique, vel nesciunt soluta provident
+        recusandae possimus dignissimos eaque.`,
+  },
+];
