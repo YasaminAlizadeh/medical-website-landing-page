3abb7c02f4dc37a5df5a7775986726c924961cd1
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-05-07 01:47:04 +0430

    feat(slider): initialized third slider

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,5 +1,6 @@
 let splide1;
 let splide2;
+let splide3;
 
 document.addEventListener("DOMContentLoaded", function () {
   splide1 = new Splide("#splide1", {
@@ -63,13 +64,34 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   splide2.mount();
+
+  splide3 = new Splide("#splide3", {
+    perPage: 1,
+    perMove: 1,
+    autoplay: true,
+    interval: 5000,
+    start: 0,
+    rewind: true,
+    rewindByDrag: true,
+    gap: "1rem",
+    snap: true,
+    wheel: true,
+    waitForTransition: true,
+    classes: {
+      pagination: "splide__pagination pagination__bar",
+      page: "splide__pagination__page pagination__page",
+      arrows: "splide__arrows slider__arrows",
+      arrow: "splide__arrow slider__arrow",
+      prev: "splide__arrow--prev slider__arrow--prev",
+      next: "splide__arrow--next slider__arrow--next",
+    },
+  });
+
+  splide3.mount();
 });
 
 const slider1 = document.querySelector("#slider1");
 const sliderContent1 = document.querySelector("#splide1");
-
-const slider2 = document.querySelector("#slider2");
-const sliderContent2 = document.querySelector("#splide2");
 
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
