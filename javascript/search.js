9ea3f92f23637dc0a2113cefe95dade311c7d02f
Yasamin Alizadeh <itsYasaminAlizadeh@gmail.com>
2022-05-06 13:01:42 +0430

    feat(search): preventDefault second search form

--- javascript/search.js
+++ javascript/search.js
@@ -1,5 +1,10 @@
 const searchForm = document.querySelector("#search-form");
+const searchForm2 = document.querySelector("#search-form-2");
 
 searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+searchForm2.addEventListener("submit", (e) => {
   e.preventDefault();
 });
